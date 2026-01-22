d128a6a858bf036ca5ce8b33123a2500efb67206
misha <kiritosan08anime@gmail.com>
2026-01-22 17:22:29 +0100

    newww

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./components/App/App";
 import "modern-normalize/modern-normalize.css";
 
-
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
